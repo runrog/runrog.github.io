3b6b210172d4d99f174c7deccdb18130aa3b4ff9
roge5716 <roger.avalos@rackspace.com>
2017-09-22 11:41:19 -0500

    update template and env

---
+++ js/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+$(document).ready(function () {
+  $('.drzNav').drzNav();
+  $('.drzAnchor-scroll').drzAnchorScroll();
+  $('.drzImageGallery').drzImageGallery();
+  $(window).topScroll();
+});
